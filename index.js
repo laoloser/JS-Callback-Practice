46f5c713463f081aab88811a376bde6118a04def
laoloser <slaux86@gmail.com>
2024-03-22 23:21:54 -0700

    commit

--- index.js
+++ index.js
@@ -1,8 +1,170 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
+
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+/*if the character is moving in a certain direction 
+this code will replace the sprite with the corresponding sprite*/
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+// const character = newImage('assets/green-character/static.gif')
+// move(character).withArrowKeys(100,250)
+
+// move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+/*
+function moveCharacter() {
+if (direction === 'west') {
+    x = x - 1
+}
+
+if (direction === 'north') {
+    y = y + 1
+}
+
+if (direction === 'east') {
+    x = x + 1
+}
+if (direction === 'south') {
+    y = y -1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+}
+
+//callsback to "moveCharacter" and runs every milisecond
+setInterval(moveCharacter, 1)
+
+
+//checks every 1ms, defines which direction the asset will move
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+
+//looks for keypresses
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight') {
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+//releaseing the arrowkey (keyup) will reset direction to null
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
+*/
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, directionChange){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            if(direction === 'south'){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            directionChange(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            directionChange(direction)
+        })
+    }
+    
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
+}
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
